d7352b51a5057f8eea076fb122eedc5eccbd31df
Jaremy Creechley <creechley@gmail.com>
2021-12-11 15:31:02 -0800

    setting up tests

---
+++ tests/test1.nim
@@ -0,0 +1,5 @@
+import unittest
+
+import keyxn
+test "can add":
+  check add(5, 5) == 10
--- tests/test_shamir_arithmetic.nim
+++ tests/test_shamir_arithmetic.nim
@@ -1,65 +1,58 @@
-defmodule KeyX.Shamir.ArithmeticTest do
-  use ExUnit.Case
-  # doctest KeyX
 
-  alias KeyX.Shamir.Arithmetic
+import unittest
 
-  test "pfield add" do
+import keyxn/shamir_arithmetic
 
-    assert Arithmetic.+(16,16) == 0
-    assert Arithmetic.+(3,4) == 7
-    # assert Arithmetic.+(3,4) == 7
-  end
+test "can add":
+  check add(5, 5) == 10
 
-  test "pfield mult" do
-    assert Arithmetic.*(3,7) == 9
-    assert Arithmetic.*(3,0) == 0
-    assert Arithmetic.*(0,3) == 0
-  end
 
-  test "pfield div" do
-    assert Arithmetic./(0,7) == 0
-    assert Arithmetic./(3,3) == 1
-    assert Arithmetic./(6,3) == 2
-  end
+test "pfield add"
 
-  test "random polynomial? " do
-    poly = Arithmetic.polynomial(42, 2)
+  assert Arithmetic.+(16,16) == 0
+  assert Arithmetic.+(3,4) == 7
+  # assert Arithmetic.+(3,4) == 7
 
-    assert [42 | res] = poly
-    assert length(res) == 2
-  end
+test "pfield mult":
+  assert Arithmetic.*(3,7) == 9
+  assert Arithmetic.*(3,0) == 0
+  assert Arithmetic.*(0,3) == 0
 
-  test "poly evaluate simple" do
+test "pfield div":
+  assert Arithmetic./(0,7) == 0
+  assert Arithmetic./(3,3) == 1
+  assert Arithmetic./(6,3) == 2
 
-    poly = Arithmetic.polynomial(42, 1)
+test "random polynomial? ":
+  poly = Arithmetic.polynomial(42, 2)
 
-    assert Arithmetic.evaluate(poly, 0) == 42
-  end
+  assert [42 | res] = poly
+  assert length(res) == 2
 
-  test "poly evaluate advanced" do
+test "poly evaluate simple":
 
-    poly = Arithmetic.polynomial(42, 1)
+  poly = Arithmetic.polynomial(42, 1)
 
-    res_poly = Arithmetic.evaluate(poly, 1)
-    res_exp = Arithmetic.+(42, Arithmetic.*(1, Enum.at(poly,1) ))
+  assert Arithmetic.evaluate(poly, 0) == 42
 
-    assert res_poly == res_exp
-  end
+test "poly evaluate advanced":
 
-  test "poly random" do
+  poly = Arithmetic.polynomial(42, 1)
 
-    for i <- 0..255 do
+  res_poly = Arithmetic.evaluate(poly, 1)
+  res_exp = Arithmetic.+(42, Arithmetic.*(1, Enum.at(poly,1) ))
 
-      poly = Arithmetic.polynomial(i,2)
+  assert res_poly == res_exp
 
-      x_vals = [1, 2, 3]
-      y_vals = x_vals |> Enum.map(&(Arithmetic.evaluate(poly, &1)))
+test "poly random":
 
-      out = Arithmetic.interpolate(x_vals, y_vals, 0)
+  for i <- 0..255:
 
-      assert out == i
-    end
-  end
+    poly = Arithmetic.polynomial(i,2)
 
-end
+    x_vals = [1, 2, 3]
+    y_vals = x_vals |> Enum.map(&(Arithmetic.evaluate(poly, &1)))
+
+    out = Arithmetic.interpolate(x_vals, y_vals, 0)
+
+    assert out == i
