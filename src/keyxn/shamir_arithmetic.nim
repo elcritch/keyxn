06ac8356defd4e40a9db9c6f3ea12f1c2b9904b2
Jaremy Creechley <creechley@gmail.com>
2021-12-11 17:13:53 -0800

    Initial port of Shamir's secret

---
+++ src/keyxn/shamir_arithmetic.nim
@@ -0,0 +1,84 @@
+import std/sysrand
+import strutils
+import sequtils
+
+import tables
+
+type
+  gfInt8* = distinct uint8
+  Polynomial* = object
+    coefs: seq[gfInt8]
+
+proc log*(x: gfInt8): gfInt8 {.inline.} =
+  result = gfint8 tables.log_over_log_table[x.uint8]
+
+proc exp*(x: gfInt8): gfint8 {.inline.} =
+  result = gfint8 tables.anti_log_table[x.uint8]
+
+proc `+`*(x, y: gfInt8): gfInt8 =
+  var val: int = x.int + y.int 
+  result = gfInt8(val mod 255)
+
+proc `-`*(x, y: gfInt8): gfInt8 =
+  var val: int = x.int - y.int
+  result = gfInt8((val + 255) mod 255)
+
+proc `==`*(x, y: gfInt8): bool {.borrow.}
+
+proc `'g8`*(n: string): gfInt8 =
+  # The leading ' is required.
+  result = (parseInt(n) and 0xFF).gfInt8()
+
+proc `div`*(lhs, rhs: gfint8): gfint8 =
+  var
+    zero: gfInt8 = 0'g8
+    ret: gfInt8 = exp(log(lhs) - log(rhs))
+  # done for timing purposes
+  if lhs == 0'g8: zero else: ret
+
+# Multiplies two numbers in GF(2^8)
+proc `*`*(lhs, rhs: gfInt8): gfInt8 =
+  var
+    zero = 0'g8
+    ret = exp(log(lhs) + log(rhs))
+  # done for timing purposes
+  if lhs == 0'g8 or rhs == 0'g8: zero else: ret
+
+proc randPolynomial*(intercept: gfInt8, degree: int): Polynomial =
+  result.coefs = newSeq[gfInt8](degree+1)
+  result.coefs[1..^1] = urandom(degree).mapIt(it.gfInt8()) 
+
+# @spec evaluate(polynomial, non_neg_integer) :: non_neg_integer
+proc evaluate*(poly: Polynomial, x: gfInt8): gfInt8 =
+  if x == 0'g8:
+    result = poly.coefs[0]
+  else:
+    # Use Horner's method,
+    # essentially multiply `x` by itself `n` times while adding each
+    # lower order coefficient as the 'intercept' each time 
+    # ... or something ;)
+    let n = poly.coefs.high - 1
+    result = poly.coefs[^1]
+    for i in countdown(n, 0):
+      result = result * x + poly.coefs[i]
+
+proc `[]`*(poly: Polynomial, x: gfInt8): gfInt8 =
+  result = poly.evaluate(x)
+
+# interp
+proc interpolate*(x_samples, y_samples: seq[gfInt8], x: gfInt8): gfInt8 =
+  # Setup interpolation env
+  assert x_samples.len() == y_samples.len()
+  var limit = len(x_samples) - 1
+
+  # Loop through all the x & y samples, reducing them to an answer
+  for i in 0..limit:
+    var basis = 1'g8
+    for j in 0..limit:
+      if i == j:
+        continue
+      basis = basis * (x + x_samples[j]) div (x_samples[i] + x_samples[j])
+
+    let group = basis * y_samples[i]
+    result = result + group
+
--- tests/test_shamir_arithmetic.nim
+++ tests/test_shamir_arithmetic.nim
@@ -7,33 +7,32 @@ test "can add":
   check add(5, 5) == 10
 
 
-test "pfield add"
-
-  assert Arithmetic.+(16,16) == 0
-  assert Arithmetic.+(3,4) == 7
-  # assert Arithmetic.+(3,4) == 7
+test "pfield add":
+  check Arithmetic.+(16,16) == 0
+  check Arithmetic.+(3,4) == 7
+  # check Arithmetic.+(3,4) == 7
 
 test "pfield mult":
-  assert Arithmetic.*(3,7) == 9
-  assert Arithmetic.*(3,0) == 0
-  assert Arithmetic.*(0,3) == 0
+  check Arithmetic.*(3,7) == 9
+  check Arithmetic.*(3,0) == 0
+  check Arithmetic.*(0,3) == 0
 
 test "pfield div":
-  assert Arithmetic./(0,7) == 0
-  assert Arithmetic./(3,3) == 1
-  assert Arithmetic./(6,3) == 2
+  check Arithmetic./(0,7) == 0
+  check Arithmetic./(3,3) == 1
+  check Arithmetic./(6,3) == 2
 
 test "random polynomial? ":
   poly = Arithmetic.polynomial(42, 2)
 
-  assert [42 | res] = poly
-  assert length(res) == 2
+  check [42 | res] = poly
+  check length(res) == 2
 
 test "poly evaluate simple":
 
   poly = Arithmetic.polynomial(42, 1)
 
-  assert Arithmetic.evaluate(poly, 0) == 42
+  check Arithmetic.evaluate(poly, 0) == 42
 
 test "poly evaluate advanced":
 
@@ -42,7 +41,7 @@ test "poly evaluate advanced":
   res_poly = Arithmetic.evaluate(poly, 1)
   res_exp = Arithmetic.+(42, Arithmetic.*(1, Enum.at(poly,1) ))
 
-  assert res_poly == res_exp
+  check res_poly == res_exp
 
 test "poly random":
 
@@ -55,4 +54,4 @@ test "poly random":
 
     out = Arithmetic.interpolate(x_vals, y_vals, 0)
 
-    assert out == i
+    check out == i
