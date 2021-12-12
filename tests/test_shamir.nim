55017eac431a031fb7674dcb5a7f8c8a31145eab
Jaremy Creechley <creechley@gmail.com>
2021-12-12 02:30:42 -0800

    adding in more tests

--- tests/test_shamir.nim
+++ tests/test_shamir.nim
@@ -30,3 +30,19 @@ test "basic recover static long":
   let
     res = shares.recover()
   check "test" == res.data
+
+test "basic split & recover short":
+  let secret = ShamirSecret(data: "t")
+  let shares = secret.split(2, 2)
+
+  let res = shares.recover()
+  check secret == res
+
+test "basic split & recover long":
+  let secret = ShamirSecret(data: "super secret")
+  let shares = secret.split(2, 4)
+
+  let
+    res = shares[0..1].recover()
+
+  check secret == res
